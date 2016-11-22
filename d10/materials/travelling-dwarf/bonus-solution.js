0cf388f7e2171fdaaee4e0d52e35e9d5eb6bc529
marladekker <marladekker@Marlas-MacBook-Pro.local>
2016-11-22 18:35:36 -0500

    Week 5 Homework

---
+++ d10/materials/travelling-dwarf/bonus-solution.js
@@ -0,0 +1,29 @@
+console.log
+
+// var x = 0;
+// var y = 0;
+
+// while (x && y < 19) {
+// 	d.move ('right');
+// 	x = (x + 1) || y = (y + 1);
+// 	} else {
+// 		d.orientation === d.x || d.y;
+// 		d.orient ('right') || d.orient ('down');
+// 	}
+
+
+
+var x = 0;
+var y = 0;
+
+if (x < 5 || y < 19) {
+	d.move ('right');
+	x = (x + 1) && y = (y + 1);
+} else {
+	d.orient ('right') || d.orient ('down');
+}	
+
+
+	
+
+
---
+++ d10/materials/travelling-dwarf/solution.js
@@ -0,0 +1,67 @@
+console.log("WRITE YOUR SOLUTION IN HERE!");
+d.move ('right');
+d.move ('right');
+d.move ('right');
+d.move ('right');
+d.orient ('down');
+d.move ('right');
+d.move ('right');
+d.move ('right');
+d.move ('right');
+d.move ('right');
+d.move ('right');
+d.move ('right');
+d.move ('right');
+d.move ('right');
+d.move ('right');
+d.move ('right');
+d.move ('right');
+d.move ('right');
+d.move ('right');
+d.move ('right');
+d.move ('right');
+d.orient ('right');
+d.move ('right');
+d.move ('right');
+d.move ('right');
+d.move ('right');
+d.move ('right');
+d.move ('right');
+d.orient ('up');
+d.move ('right');
+d.move ('right');
+d.move ('right');
+d.move ('right');
+d.move ('right');
+d.move ('right');
+d.move ('right');
+d.move ('right');
+d.move ('right');
+d.move ('right');
+d.move ('right');
+d.orient ('right');
+d.move ('right');
+d.move ('right');
+d.move ('right');
+d.move ('right');
+d.move ('right');
+d.move ('right');
+d.orient ('down');
+d.move ('right');
+d.move ('right');
+d.move ('right');
+d.move ('right');
+d.move ('right');
+d.move ('right');
+d.move ('right');
+d.move ('right');
+d.move ('right');
+d.orient ('right');
+d.move ('right');
+d.move ('right');
+
+
+
+
+
+
